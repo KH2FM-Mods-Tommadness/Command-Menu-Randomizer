3e2713026deea844f306071f000abef03b9ea9d8
tommadness <tom.m.cover@gmail.com>
2021-03-07 23:46:40 -0500

    Add project files.

---
+++ Random Command Menu/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using YamlDotNet.Serialization;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace Random_Command_Menu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Randomizer randomizer = new Randomizer();
+
+        }
+
+
+    }
+}
---
+++ Random Command Menu/Randomizer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+using YamlDotNet;
+
+namespace Random_Command_Menu
+{
+    class Randomizer
+    {
+        List<string> commandMenuNames;
+        public Randomizer()
+        {
+            commandMenuNames = new List<string>();
+            var input = File.ReadAllText("mod.yml");
+            foreach (var commandMenu in Directory.GetFiles("CommandMenus"))
+            {
+                commandMenuNames.Add(commandMenu);
+            }
+
+            MatchEvaluator evaluator = new MatchEvaluator(Randomize);
+            var textout = Regex.Replace(input, @"- name: ((?!field2d).*)\r", evaluator,RegexOptions.ExplicitCapture);
+
+            File.WriteAllText("mod.yml", textout);
+        }
+
+        private string Randomize(Match match)
+        {
+            Random rnd = new Random();
+            string newMenu;
+
+            if (commandMenuNames.Count > 0)
+            {
+                newMenu = commandMenuNames[rnd.Next(0, commandMenuNames.Count)];
+                commandMenuNames.Remove(newMenu);
+                return "- name: " + newMenu.Replace(@"\",@"/") + "\r";
+            }
+            return null;
+
+        }
+    }
+}
