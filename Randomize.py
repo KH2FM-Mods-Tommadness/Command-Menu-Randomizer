d50c45760ca27335442d134ddf63a802ea560fb3
tommadness <tom.m.cover@gmail.com>
2021-03-27 11:36:54 -0400

    Update randomize.py

--- Randomize.py
+++ Randomize.py
@@ -1,12 +1,12 @@
 import os
 import random
-
-files = os.listdir("CommandMenus")
-randomFiles = os.listdir("CommandMenus")
+currentDir = os.path.realpath(__file__).replace(os.path.basename(__file__),'')
+files = os.listdir(currentDir+"CommandMenus")
+randomFiles = os.listdir(currentDir+"CommandMenus")
 random.shuffle(randomFiles)
 output = "assets:\r"
 for i in range(len(files)):
     output += "- name: field2d\jp\{0}\r  method: copy\r  source:\r  - name: CommandMenus\{1}\r".format(files[i],randomFiles[i])
-f = open("mod.yml", "w")
+f = open(currentDir+"mod.yml", "w")
 f.write(output)
 f.close()
